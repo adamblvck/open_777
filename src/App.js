15cb29d927135bb64779b0232ee0b31df22c3e01
adam_blvck <contact@adamblvck.com>
2023-11-30 16:52:36 +0100

    Added a filter for Aleister's way of grouping correspondences together

--- src/App.js
+++ src/App.js
@@ -19,14 +19,33 @@ function App() {
 	const [showSearch, setShowSearch] = React.useState(false);
 	const onSearchClose = React.useCallback(() => setShowSearch(false), []);
 
+	// Add state for selected filter
+    const [selectedFilter, setSelectedFilter] = React.useState("All");
+
+	// Define the ranges for each filter
+    const filterRanges = {
+		"All": Array.from({ length: 36 }, (_, i) => i), // first column is the keyscale, then follow 0 1-32, 31b, 32b = 36 columns!
+		"The Spheres": [0, 2,3,4,5,6,7,8,9,10,11], // Rows 0 to 10
+		"The Planets": [0, 13, 14, 15, 22, 28, 31, 33], // Specific rows for Planets
+		"The Elements": [0, 12, 24, 32, 34, 35], // Specific rows for Planets
+		"The Zodiacs": [0, 16, 17, 18, 19, 20, 21, 23, 25, 26, 27, 29, 30], // Specific rows for Zodiacs
+		"The Paths": [0, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35] // Rows 11 to 32
+	};
+
 	const [columns, setColumns] = React.useState(TableColumns);
+
+	React.useEffect(() => {
+		const filteredColumns = TableColumns.filter((_, index) => filterRanges[selectedFilter].includes(index));
+		setColumns(filteredColumns);
+	}, [selectedFilter]);
 
 	const getContent = React.useCallback( cell => {
 		const [col, row] = cell;
 		const dataRow = Liber777[row];
-		// dumb but simple way to do this
-
-		const d = dataRow[ TableIndex[col]];
+	
+		// retrieve correct index, based on selected filter range
+		const filterSpecificColumn = filterRanges[selectedFilter][col]
+		const d = dataRow[ TableIndex[filterSpecificColumn]];
 
 		return {
 			kind: GridCellKind.Text,
@@ -34,7 +53,21 @@ function App() {
 			displayData: d,
 			data: d,
 		};
-	}, []);
+	}, [columns]);
+
+	const Slider = ({ options, onChange }) => (
+        <div className="selector">
+            {options.map(option => (
+                <button
+                    key={option}
+                    className={`selector-option ${selectedFilter === option ? 'selected' : ''}`}
+                    onClick={() => onChange(option)}
+                >
+                    {option}
+                </button>
+            ))}
+        </div>
+    );
 
 	const getTextColor = (background_color) => {
 		// Remove the "#" symbol if present
@@ -81,17 +114,20 @@ function App() {
 					<div className="subtitle">Online Look-up & Reference Tool for Aleister Crowley's Kabbalistic Correspondences</div>
 				</div>
 				
-				
-
 				<div className="logoContainer">
 					<img src="logo_full_icon_transparent.png" alt="Three Sevens"></img>
 				</div>
 
 				<button onClick={()=>setShowSearch(true)} type="button" className="search">
 					<div className="iconContainer">
-						<svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><path d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="#ccc" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M338.29 338.29L448 448"/></svg>
+						<svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><path d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="32"/><path fill="none" stroke="#ccc" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32" d="M338.29 338.29L448 448"/></svg>
 					</div>
 				</button>
+
+				<Slider
+                    options={["All", "The Spheres", "The Planets", "The Zodiacs", "The Elements", "The Paths"]}
+                    onChange={setSelectedFilter}
+                />
 
 				<div className="dataContainer">
 					<DataEditor
@@ -108,8 +144,12 @@ function App() {
 						onSearchClose={onSearchClose}
 						className="data"
 						getCellContent={getContent}
+
 						columns={columns}
 						rows={Liber777.length}
+
+						smoothScrollY={true}
+						smoothScrollX={true}
 
 						drawCell={args => {
 							const { cell, rect, ctx } = args;
