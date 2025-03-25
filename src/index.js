8d03cac220bdb532ed42b96462dd3247a5e5597e
adam_blvck <contact@adamblvck.com>
2025-03-26 00:08:13 +0100

    Fighting service workers

--- src/index.js
+++ src/index.js
@@ -24,6 +24,6 @@ if ('serviceWorker' in navigator) {
       console.log('Service Worker registered with scope:', registration.scope);
     })
     .catch((error) => {
-      console.error('Service Worker registration failed:', error);
+      console.error('Service Worker registration failed:', error, "in scope", scope);
     });
 }
